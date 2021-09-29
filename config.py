062bc388f1a9067431f2403060118ca088921850
mehmetalisa <83994095+mehmetalisa@users.noreply.github.com>
2021-09-29 21:47:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ load_dotenv()
 admins = {}
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 API_ID = int(getenv("API_ID", "6"))
-API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e"))
+API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 SESSION_NAME = getenv("SESSION_NAME", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
